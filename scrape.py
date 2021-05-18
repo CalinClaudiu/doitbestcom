3d480b40b67f4ac5d671694b66f12d4d75394977
CalinClaudiu <54081267+CalinClaudiu@users.noreply.github.com>
2021-05-18 17:31:02 +0200

    test

--- scrape.py
+++ scrape.py
@@ -1,61 +1,356 @@
-from sgscrape.sgrecord import SgRecord
-from sgscrape.sgwriter import SgWriter
+from sgscrape import simple_scraper_pipeline as sp
+from sglogging import sglog
 from sgrequests import SgRequests
-from sglogging import SgLogSetup
-from bs4 import BeautifulSoup as bs
-from sgscrape.sgpostal import parse_address_intl
+from bs4 import BeautifulSoup as b4
+import asyncio
+import httpx
+import os
+import json
+import time
 
-logger = SgLogSetup().get_logger("good-sam")
-
-_headers = {
-    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36",
-}
+EXPECTED_TOTAL = 0
+DEFAULT_PROXY_URL = "http://groups-RESIDENTIAL,country-us:{}@proxy.apify.com:8000/"
+logzilla = sglog.SgLogSetup().get_logger(logger_name="Scraper")
 
 
-def fetch_data():
-    with SgRequests() as session:
-        locator_domain = "https://www.good-sam.com/"
-        base_url = "https://www.good-sam.com/coveo/rest/search/v2?sitecoreItemUri=sitecore%3A%2F%2Fweb%2F%7B7D2D77E4-CABF-4506-84FF-C3DC79854B74%7D%3Flang%3Den%26amp%3Bver%3D1&siteName=GoodSam&actionsHistory=%5B%5D&referrer=&visitorId=&isGuestUser=true&aq=(((%40fz95xpath79929%3D%3D7D2D77E4CABF450684FFC3DC79854B74%20%40fz95xtemplate79929%3D%3D57E2BFE90934466D93CD4A4405A15441)%20NOT%20%40fz95xtemplate79929%3D%3D(ADB6CA4F03EF4F47B9AC9CE2BA53FF97%2CFE5DD82648C6436DB87A7C4210C7413B)))%20(((%24qf(function%3A%20%27dist(%40flocationlatitude79929%2C%40flocationlongitude79929%2C0%2C0)%2F%201609.344%27%2C%20fieldName%3A%20%27distance%27)%20%40distance%3C100000)%20OR%20(%40fstate79929%3D%3DKS))%20OR%20(%40flocationservices79929%3DKS))&cq=(%40fz95xlanguage79929%3D%3Den)%20(%40fz95xlatestversion79929%3D%3D1)%20(%40source%3D%3D%22GoodSam_coveo_web_index%20-%20sanford-coveo-prod%22)&searchHub=GSSLocationSearch&locale=en&maximumAge=9000000&firstResult=0&numberOfResults=1000&excerptLength=200&enableDidYouMean=true&sortCriteria=distance%20ascending&queryFunctions=%5B%5D&rankingFunctions=%5B%5D&facetOptions=%7B%7D&categoryFacets=%5B%5D&retrieveFirstSentences=true"
-        locations = session.get(base_url, headers=_headers).json()
+def set_proxies():
+    if "PROXY_PASSWORD" in os.environ and os.environ["PROXY_PASSWORD"].strip():
 
-        logger.info(f"{len(locations['results'])} locations found")
+        proxy_password = os.environ["PROXY_PASSWORD"]
+        url = (
+            os.environ["PROXY_URL"] if "PROXY_URL" in os.environ else DEFAULT_PROXY_URL
+        )
+        proxy_url = url.format(proxy_password)
+        proxies = {
+            "http://": proxy_url,
+        }
+        return proxies
+    else:
+        return None
 
-        for _ in locations["results"]:
-            logger.info(_["clickUri"])
-            page_url = _["clickUri"].replace(":443", "")
-            logger.info(page_url)
-            sp1 = bs(session.get(page_url, headers=_headers).text, "lxml")
-            addr = parse_address_intl(
-                " ".join(
-                    sp1.select_one("div.location-info__info-col p").stripped_strings
+
+proxies = set_proxies()
+
+
+async def fetch_data(index: int, url: str) -> dict:
+    headers = {
+        "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36"
+    }
+    timeout = httpx.Timeout(60.0, connect=120.0)
+    data = {}
+    if len(url) > 0:
+        async with httpx.AsyncClient(
+            proxies=proxies, headers=headers, timeout=timeout
+        ) as client:
+            response = await client.get(url)
+            soup = b4(response.text, "lxml")
+            logzilla.info(f"URL\n{url}\nLen:{len(response.text)}\n\n")
+            data = json.loads(
+                str(
+                    soup.find(
+                        "script",
+                        {"type": "application/ld+json", "id": "schema-webpage"},
+                    ).text
+                )
+                .replace("\u0119", "e")
+                .replace("\u011f", "g")
+                .replace("\u0144", "n")
+                .replace("\u0131", "i"),
+                strict=False,
+            )
+            data["index"] = index
+            data["requrl"] = url
+            data["STATUS"] = True
+    else:
+        await asyncio.sleep(0)  # had to improvise
+        data["index"] = index
+        data["requrl"] = "<MISSING>"
+        data["STATUS"] = False
+    return data
+
+
+async def get_brand(brand_code, brand_name, url):
+    url = url + brand_code
+
+    headers = {}
+    headers["authority"] = "www.radissonhotels.com"
+    headers["method"] = "GET"
+    headers["path"] = "/zimba-api/destinations/hotels?brand=" + brand_code
+    headers["scheme"] = "https"
+    headers["accept"] = "application/json, text/plain, */*"
+    headers["accept-encoding"] = "gzip, deflate, br"
+    headers["accept-language"] = "en-us"
+    headers["referer"] = "https://www.radissonhotels.com/en-us/destination"
+    headers["sec-fetch-dest"] = "empty"
+    headers["sec-fetch-mode"] = "cors"
+    headers["sec-fetch-site"] = "same-origin"
+    headers[
+        "user-agent"
+    ] = "Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.66 Safari/537.36"
+
+    session = SgRequests()
+    son = session.get(url, headers=headers).json()
+    task_list = []
+    results = []
+    chunk_size = 50
+    last_chunk = 0
+    last_tick = time.monotonic()
+    total_records = len(son["hotels"])
+    global EXPECTED_TOTAL
+    EXPECTED_TOTAL += total_records
+    for index, record in enumerate(son["hotels"]):
+        task_list.append(fetch_data(index, record["overviewPath"]))
+        if index % chunk_size == 0 and last_chunk != index:
+            last_tick = time.monotonic()
+            last_chunk = index
+            if len(task_list) > 0:
+                z = await asyncio.gather(*task_list)
+                for item in z:
+                    results.append(
+                        {
+                            "main": son["hotels"][item["index"]],
+                            "sub": item,
+                            "@type": brand_name,
+                        }
+                    )
+                logzilla.info(
+                    f"Finished {last_chunk}/{total_records} for brand {brand_name}, last step took {round(time.monotonic()-last_tick,5)} seconds."
+                )
+                task_list = []
+
+    last_tick = time.monotonic()
+    if len(task_list) > 0:
+        z = await asyncio.gather(*task_list)
+        for item in z:
+            results.append(
+                {"main": son["hotels"][item["index"]], "sub": item, "@type": brand_name}
+            )
+        logzilla.info(
+            f"Finished {total_records}/{total_records} for brand {brand_name}, last step took {round(time.monotonic()-last_tick,5)} seconds."
+        )
+    return results
+
+
+def clean_record(k):
+    try:
+        k["sub"] = k["sub"]
+    except Exception:
+        k["sub"] = {}
+        k["sub"]["mainEntity"] = {}
+        k["sub"]["mainEntity"]["address"] = {}
+        k["sub"]["mainEntity"]["address"]["streetAddress"] = "<MISSING>"
+        k["sub"]["mainEntity"]["address"]["addressLocality"] = "<MISSING>"
+        k["sub"]["mainEntity"]["address"]["addressRegion"] = "<MISSING>"
+        k["sub"]["mainEntity"]["address"]["postalCode"] = "<MISSING>"
+        k["sub"]["mainEntity"]["address"]["addressCountry"] = "<MISSING>"
+        k["sub"]["mainEntity"]["telephone"] = ["<MISSING>"]
+        k["sub"]["mainEntity"]["@type"] = "<MISSING>"
+
+    try:
+        k["sub"]["mainEntity"] = k["sub"]["mainEntity"]
+    except Exception:
+        k["sub"]["mainEntity"] = {}
+        k["sub"]["mainEntity"]["address"] = {}
+        k["sub"]["mainEntity"]["address"]["streetAddress"] = "<MISSING>"
+        k["sub"]["mainEntity"]["address"]["addressLocality"] = "<MISSING>"
+        k["sub"]["mainEntity"]["address"]["addressRegion"] = "<MISSING>"
+        k["sub"]["mainEntity"]["address"]["postalCode"] = "<MISSING>"
+        k["sub"]["mainEntity"]["address"]["addressCountry"] = "<MISSING>"
+        k["sub"]["mainEntity"]["telephone"] = ["<MISSING>"]
+        k["sub"]["mainEntity"]["@type"] = "<MISSING>"
+
+    try:
+        k["sub"]["mainEntity"]["address"] = k["sub"]["mainEntity"]["address"]
+    except Exception:
+        k["sub"]["mainEntity"]["address"] = {}
+        k["sub"]["mainEntity"]["address"]["streetAddress"] = "<MISSING>"
+        k["sub"]["mainEntity"]["address"]["addressLocality"] = "<MISSING>"
+        k["sub"]["mainEntity"]["address"]["addressRegion"] = "<MISSING>"
+        k["sub"]["mainEntity"]["address"]["postalCode"] = "<MISSING>"
+        k["sub"]["mainEntity"]["address"]["addressCountry"] = "<MISSING>"
+
+    try:
+        k["sub"]["mainEntity"]["address"]["streetAddress"] = k["sub"]["mainEntity"][
+            "address"
+        ]["streetAddress"]
+    except Exception:
+        k["sub"]["mainEntity"]["address"]["streetAddress"] = "<MISSING>"
+
+    try:
+        k["sub"]["mainEntity"]["address"]["addressLocality"] = k["sub"]["mainEntity"][
+            "address"
+        ]["addressLocality"]
+    except Exception:
+        k["sub"]["mainEntity"]["address"]["addressLocality"] = "<MISSING>"
+
+    try:
+        k["sub"]["mainEntity"]["address"]["addressRegion"] = k["sub"]["mainEntity"][
+            "address"
+        ]["addressRegion"]
+    except Exception:
+        k["sub"]["mainEntity"]["address"]["addressRegion"] = "<MISSING>"
+
+    try:
+        k["sub"]["mainEntity"]["address"]["postalCode"] = k["sub"]["mainEntity"][
+            "address"
+        ]["postalCode"]
+    except Exception:
+        k["sub"]["mainEntity"]["address"]["postalCode"] = "<MISSING>"
+
+    try:
+        k["sub"]["mainEntity"]["address"]["addressCountry"] = k["sub"]["mainEntity"][
+            "address"
+        ]["addressCountry"]
+    except Exception:
+        k["sub"]["mainEntity"]["address"]["addressCountry"] = "<MISSING>"
+
+    try:
+        k["sub"]["mainEntity"]["telephone"] = k["sub"]["mainEntity"]["telephone"]
+    except Exception:
+        k["sub"]["mainEntity"]["telephone"] = "<MISSING>"
+
+    try:
+        k["sub"]["mainEntity"]["@type"] = k["sub"]["mainEntity"]["@type"]
+    except Exception:
+        k["sub"]["mainEntity"]["@type"] = ""
+
+    try:
+        k["main"] = k["main"]
+    except Exception:
+        k["main"] = {}
+        k["main"]["coordinates"] = {}
+        k["main"]["coordinates"]["latitude"] = "<MISSING>"
+        k["main"]["coordinates"]["longitude"] = "<MISSING>"
+        k["main"]["code"] = "<MISSING>"
+        k["main"]["name"] = "<MISSING>"
+
+    try:
+        k["main"]["coordinates"] = k["main"]["coordinates"]
+    except Exception:
+        k["main"]["coordinates"] = {}
+        k["main"]["coordinates"]["latitude"] = "<MISSING>"
+        k["main"]["coordinates"]["longitude"] = "<MISSING>"
+
+    try:
+        k["main"]["code"] = k["main"]["code"]
+    except Exception:
+        k["main"]["code"] = "<MISSING>"
+
+    try:
+        k["main"]["name"] = k["main"]["name"]
+    except Exception:
+        k["main"]["name"] = "<MISSING>"
+
+    return k
+
+
+def start():
+    urls = [
+        "https://www.radissonhotelsamericas.com/zimba-api/destinations/hotels?brand=",
+        "https://www.radissonhotels.com/zimba-api/destinations/hotels?brand=",
+    ]
+    brands = [
+        {"code": "pii", "name": "Park Inn by Radisson"},
+        {"code": "rdb", "name": "Radisson Blu"},
+        {"code": "rdr", "name": "Radisson RED"},
+        {"code": "art", "name": "art'otel"},
+        {"code": "rad", "name": "Radisson"},
+        {"code": "ri", "name": "Radisson Individuals"},
+        {"code": "prz", "name": "prizeotel"},
+        {"code": "pph", "name": "Park Plaza"},
+        {"code": "cis", "name": "Country Inn & Suites"},
+        {"code": "rco", "name": "Radisson Collection"},
+    ]
+    badrecords = []
+    for url in urls:
+        for brand in brands:
+            start_time = time.monotonic()
+            logzilla.info(f"Selected brand: {brand}")
+            data = asyncio.run(
+                get_brand(
+                    brand["code"],
+                    brand["name"],
+                    "https://www.radissonhotels.com/zimba-api/destinations/hotels?brand=",
                 )
             )
-            street_address = addr.street_address_1
-            if addr.street_address_2:
-                street_address += " " + addr.street_address_2
-            coord = (
-                sp1.select_one("div.swiper-slide iframe")["src"]
-                .split("&q=")[1]
-                .split("&z")[0]
-                .split(",")
+            for i in data:
+                k = clean_record(i)
+                if k["sub"]["STATUS"]:
+                    yield k
+                else:
+                    badrecords.append(k)
+                    yield k
+            logzilla.info(
+                f"Finished brand {brand['name']}, it took {round(time.monotonic()-start_time,5)}\n it has {len(data)} locations"
             )
-            yield SgRecord(
-                page_url=page_url,
-                location_name=_["Title"],
-                street_address=street_address,
-                city=addr.city,
-                state=addr.state,
-                zip_postal=addr.postcode,
-                country_code="US",
-                latitude=coord[0],
-                longitude=coord[1],
-                phone=sp1.select_one("div.location-info__info-col h4").text,
-                locator_domain=locator_domain,
-            )
+    logzilla.info(f"Badrecords :\n\n{badrecords}")
+    global EXPECTED_TOTAL
+    logzilla.info(f"Finished grabbing data!!\n expected total {EXPECTED_TOTAL}")  # noqa
+
+
+def scrape():
+    url = "https://www.radissonhotels.com/en-us/destination"
+    field_defs = sp.SimpleScraperPipeline.field_definitions(
+        locator_domain=sp.ConstantField(url),
+        page_url=sp.MappingField(
+            mapping=["sub", "requrl"],
+            is_required=False,
+        ),
+        location_name=sp.MappingField(
+            mapping=["main", "name"],
+            is_required=False,
+        ),
+        latitude=sp.MappingField(
+            mapping=["main", "coordinates", "latitude"],
+            is_required=False,
+        ),
+        longitude=sp.MappingField(
+            mapping=["main", "coordinates", "longitude"],
+            is_required=False,
+        ),
+        street_address=sp.MappingField(
+            mapping=["sub", "mainEntity", "address", "streetAddress"],
+            is_required=False,
+        ),
+        city=sp.MappingField(
+            mapping=["sub", "mainEntity", "address", "addressLocality"],
+            is_required=False,
+        ),
+        state=sp.MappingField(
+            mapping=["sub", "mainEntity", "address", "addressRegion"],
+            is_required=False,
+        ),
+        zipcode=sp.MappingField(
+            mapping=["sub", "mainEntity", "address", "postalCode"], is_required=False
+        ),
+        country_code=sp.MappingField(
+            mapping=["sub", "mainEntity", "address", "addressCountry"],
+            is_required=False,
+        ),
+        phone=sp.MappingField(
+            mapping=["sub", "mainEntity", "telephone", 0],
+            is_required=False,
+        ),
+        store_number=sp.MappingField(
+            mapping=["main", "code"],
+            is_required=False,
+        ),
+        hours_of_operation=sp.MissingField(),
+        location_type=sp.MappingField(
+            mapping=["@type"],
+            is_required=False,
+        ),
+    )
+
+    pipeline = sp.SimpleScraperPipeline(
+        scraper_name="pipeline",
+        data_fetcher=start,
+        field_definitions=field_defs,
+        log_stats_interval=30,
+    )
+
+    pipeline.run()
 
 
 if __name__ == "__main__":
-    with SgWriter() as writer:
-        results = fetch_data()
-        for rec in results:
-            writer.write_row(rec)
+    scrape()
